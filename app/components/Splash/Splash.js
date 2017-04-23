32325dc8fb11d30dcb6d34cd16ec45b6996c1a68
Tayef-Shah <tayefshah@gmail.com>
2017-04-23 01:23:41 -0400

    Added navigator for IOS
    Added navigator to transition between screens. Javascript code is also
    compatible with android

---
+++ app/components/Splash/Splash.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button
+
+} from 'react-native';
+
+class Splash extends Component{
+  constructor(){
+    super()
+
+    this.navigate = this.navigate.bind(this)
+  }
+
+  navigate(name)
+  {
+    this.props.navigator.push({name})
+  }
+render(){
+    return(
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            President
+          </Text>
+          <Button
+            title="Pass and Play"
+            style={styles.button}
+            onPress={() => this.navigate('gamePage')}
+          />
+          <Image source={require('../../images/MiniSuits.jpg')} style={styles.startImage}/>
+        </View>
+      );
+    }
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  startImage: {
+    flex: 1,
+    width: 500,
+    height: 100,
+    margin: 0,
+    resizeMode: 'contain'
+  },
+});
+
+export default Splash
