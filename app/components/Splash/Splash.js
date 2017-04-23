1a5d81a9ce1ce2bcbcb88438000924fd2fcd1bc4
ericpang777 <eric.pang@live.com>
2017-04-23 15:32:24 -0400

    Forgot splash style

--- app/components/Splash/Splash.js
+++ app/components/Splash/Splash.js
@@ -15,6 +15,8 @@ import {
 
 } from 'react-native';
 
+import styles from '../Splash/styles.js'
+
 class Splash extends Component{
   constructor(){
     super()
@@ -43,30 +45,5 @@ render(){
     }
   }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  welcome: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  startImage: {
-    flex: 1,
-    width: 500,
-    height: 100,
-    margin: 0,
-    resizeMode: 'contain'
-  },
-});
 
 export default Splash
