a03278955a000ca41e5e3e3770f7e548829d5a66
Tayef-Shah <tayefshah@gmail.com>
2017-05-22 13:40:43 -0400

    Update deck code

--- app/components/Card/Card.js
+++ app/components/Card/Card.js
@@ -1,14 +1,16 @@
 
 import React, { Component } from 'react';
+import {image} from 'react-native';
 
 export default class Card extends Component {
-  constructor() {
+  constructor(value:number,suit:String,cardName:String) {
     super()
     //generate a random number(int) and suit(String)
     //OR we can just have a deck object and randomly order the cards in that object, then we can just give out (numCards) to each player
     //return the image= num + "_of_" + suit + ".png"
   }
   render() {
+    <Image source={require('../../images/cards/' + cardName + '_of_' + suit + '.png')}/>
 
   }
 }
--- app/components/Game/Game.js
+++ app/components/Game/Game.js
@@ -6,17 +6,64 @@ import {
   Text,
   View,
   Image,
-  Button
+  Button,
 } from 'react-native';
 
 import styles from '../Game/styles.js'
+import Card from '../Card/Card.js'
 
 class Game extends Component {
   constructor(props)
   {
     super(props)
+
+      var deck = new Array(54)
+      var cardName:String
+      var value:number
+      var hearts:Card
+      var spades:Card
+      var diamonds:Card
+      var clubs:Card
+
+      for(var i = 1; i < 14; i++){
+      switch (i) {
+        case 1:
+          cardName = "A"
+          value = 12
+          break;
+        case 2:
+          cardName = "2"
+          value = 13
+          break;
+        case 11:
+          cardName = "J"
+          value = 9
+          break;
+        case 12:
+          cardName = "Q"
+          value = 10
+          break;
+        case 13:
+          cardName = "K"
+          value = 11
+          break;
+        default:
+          cardName = "" + i
+          value = i-2
+      }
+      hearts = new Card(value,"hearts",cardName)
+      spades = new Card(value,"spades",cardName)
+      diamonds = new Card(value,"diamonds",cardName)
+      clubs = new Card(value,"clubs",cardName)
+      deck.concat(hearts,spades,diamonds,clubs)
+    }
+    var joker = new Card(14,"black","joker")
+    deck.concat(joker)
+    joker = new Card(14,"red","joker")
+    deck.concat(joker)
   }
   render() {
+    var card = new Card(10,"clubs","10")
     return (
       <View style={styles.container}>
         <Text style={styles.table}>
