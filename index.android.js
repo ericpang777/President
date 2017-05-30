95b6ff72e94d92efbd6ac97e33cef2c7afa6de2c
Tayef-Shah <tayefshah@gmail.com>
2017-05-30 17:13:42 -0400

    Set random cards to hand

--- index.android.js
+++ index.android.js
@@ -21,8 +21,21 @@ export default class President extends Component {
     var cards = [
       [12, 0],[12, 1],[12, 2],[12, 3],
       [13, 4],[13, 5],[13, 6],[13, 7],
-      [1, 8]
+      [1, 8],[1, 9],[1, 10],[1, 11],
+      [2, 12],[2, 13],[2, 14],[2, 15],
+      [3, 16],[3, 17],[3, 18],[3, 19],
+      [4, 20],[4, 21],[4, 22],[4, 23],
+      [5, 24],[5, 25],[5, 26],[5, 27],
+      [6, 28],[6, 29],[6, 30],[6, 31],
+      [7, 32],[7, 33],[7, 34],[7, 35],
+      [8, 36],[8, 37],[8, 38],[8, 39],
+      [9, 40],[9, 41],[9, 42],[9, 43],
+      [10, 44],[10, 45],[10, 46],[10, 47],
+      [11, 48],[11, 49],[11, 50],[11, 51],
+      [14, 52],[14, 53]
     ]
+    cards.sort(function(a, b){return 0.5 - Math.random()});
+    var playerNames = ["Player1","Player2","Player3","Player4",]
     this.state = {
       deck : [
         [12, 0],[12, 1],[12, 2],[12, 3],
@@ -40,6 +53,7 @@ export default class President extends Component {
         [11, 48],[11, 49],[11, 50],[11, 51],
         [14, 52],[14, 53]
       ],
+      //Could also say deck : cards ?
       /*
       p1Hand: [],
       p2Hand: [],
@@ -51,25 +65,14 @@ export default class President extends Component {
         <Player hand={this.state.p3Hand} name="Player3"/>,
         <Player hand={this.state.p4Hand} name="Player4"/>
       ],*/
-      currentPlayerName: "Player1",
       currentPlayerNum: 1,
+      currentPlayerName: playerNames[0],
       lastPlayedCards : [
 
       ],
       remainingPlayers : [1,2,3,4],
       gameEnd : false,
-      /*hand : [
-          <Card value={12} imageIndex={0} overlap = {-4}/>,
-          <Card value={12} imageIndex={1} overlap = {-3}/>,
-          <Card value={12} imageIndex={2} overlap = {-2}/>,
-          <Card value={12} imageIndex={3} overlap = {-1}/>,
-          <Card value={13} imageIndex={4} overlap = {0}/>,
-          <Card value={13} imageIndex={5} overlap = {1}/>,
-          <Card value={13} imageIndex={6} overlap = {2}/>,
-          <Card value={13} imageIndex={7} overlap = {3}/>,
-          <Card value={1} imageIndex={8} overlap = {4}/>
-        ]*/
-        hand: this.getHand(cards)
+        hand: this.getHand(cards.splice(0,9))
     }
   }
 
